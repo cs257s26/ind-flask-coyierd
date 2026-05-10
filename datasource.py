6d0c60ac97f959df6e09b850c22631ab7395866a
Dashiell Coyier <dashiellcoyier@Dashiells-MacBook-Pro-54.local>
2026-05-10 16:21:51 -0500

    datasource query update

--- datasource.py
+++ datasource.py
@@ -45,8 +45,11 @@ def get_sightings_at_stop_for_year(connection, bird, stop, year) -> int:
         cursor = connection.cursor()
         stop_var = "stop_" + str(stop)
         year = str(year)
-        query = """SELECT stop_var FROM year;"""
-        cursor.execute(query, (stop_var, year, bird))
+
+        # query = """SELECT stop_var FROM year;"""
+        # cursor.execute(query, (stop_var, year, bird))
+        query = "RAISE NOTICE year"
+        cursor.execute(query, (year,))
         return cursor.fetchall()
 
     except Exception as e:
