e6363fe3564cdb5ddf720ba3d22f18cb2118973d
Dashiell Coyier <dashiellcoyier@Dashiells-MacBook-Pro-54.local>
2026-05-10 16:54:56 -0500

    datasource query update

--- datasource.py
+++ datasource.py
@@ -46,7 +46,7 @@ def get_sightings_at_stop_for_year(connection, bird, stop, year) -> list:
 
         query = 'SELECT * FROM "%s" WHERE bird_name=%s;'
         cursor.execute(query, (year, bird))
-        return cursor.fetchall()[0][stop]
+        return [cursor.fetchall()[0][stop]]
 
     except Exception as e:
         print("Something went wrong when executing the query: ", e)
