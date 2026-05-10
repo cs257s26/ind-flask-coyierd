e68b3c3285c6480f30ab6d81763bf02ce763bf6a
Dashiell Coyier <dashiellcoyier@Dashiells-MacBook-Pro-54.local>
2026-05-10 14:23:21 -0500

    using execute blocks

--- create_table.sql
+++ create_table.sql
@@ -1,79 +1,83 @@
 DO $$
-DECLARE
-    a INT;
-    b INT;
-    c INT;
 BEGIN
     FOR a IN 2020..2025 LOOP
-        DROP TABLE IF EXISTS CAST(a AS TEXT);
-        CREATE TABLE CAST(a AS TEXT) (
-        bird_name text,
-        stop_1 INT,
-        stop_2 INT,
-        stop_3 INT,
-        stop_4 INT,
-        stop_5 INT,
-        stop_6 INT,
-        stop_7 INT,
-        stop_8 INT,
-        stop_9 INT,
-        stop_10 INT,
-        stop_11 INT,
-        stop_12 INT,
-        stop_13 INT,
-        stop_14 INT,
-        stop_15 INT,
-        stop_16 INT,
-        stop_17 INT
+        EXECUTE format('DROP TABLE IF EXISTS %I', a::text);
+        EXECUTE format(
+            'CREATE TABLE %I (
+                bird_name TEXT,
+                stop_1 INT,
+                stop_2 INT,
+                stop_3 INT,
+                stop_4 INT,
+                stop_5 INT,
+                stop_6 INT,
+                stop_7 INT,
+                stop_8 INT,
+                stop_9 INT,
+                stop_10 INT,
+                stop_11 INT,
+                stop_12 INT,
+                stop_13 INT,
+                stop_14 INT,
+                stop_15 INT,
+                stop_16 INT,
+                stop_17 INT
+            )',
+            a::text
         );
     END LOOP;
 
     FOR b IN 2008..2018 LOOP
-        DROP TABLE IF EXISTS CAST(b AS TEXT);
-        CREATE TABLE CAST(b AS TEXT) (
-        bird_name text,
-        stop_1 INT,
-        stop_2 INT,
-        stop_3 INT,
-        stop_4 INT,
-        stop_5 INT,
-        stop_6 INT,
-        stop_7 INT,
-        stop_8 INT,
-        stop_9 INT,
-        stop_10 INT,
-        stop_11 INT,
-        stop_12 INT,
-        stop_13 INT,
-        stop_14 INT,
-        stop_15 INT,
-        stop_16 INT,
-        stop_17 INT
-        );
+        EXECUTE format('DROP TABLE IF EXISTS %I', b::text);
+        EXECUTE format(
+            'CREATE TABLE %I (
+                bird_name TEXT,
+                stop_1 INT,
+                stop_2 INT,
+                stop_3 INT,
+                stop_4 INT,
+                stop_5 INT,
+                stop_6 INT,
+                stop_7 INT,
+                stop_8 INT,
+                stop_9 INT,
+                stop_10 INT,
+                stop_11 INT,
+                stop_12 INT,
+                stop_13 INT,
+                stop_14 INT,
+                stop_15 INT,
+                stop_16 INT,
+                stop_17 INT
+            )',
+            b::text
+    );
     END LOOP;
 
     FOR c IN 2000..2004 LOOP
-        DROP TABLE IF EXISTS CAST(c AS TEXT);
-        CREATE TABLE CAST(c AS TEXT) (
-        bird_name text,
-        stop_1 INT,
-        stop_2 INT,
-        stop_3 INT,
-        stop_4 INT,
-        stop_5 INT,
-        stop_6 INT,
-        stop_7 INT,
-        stop_8 INT,
-        stop_9 INT,
-        stop_10 INT,
-        stop_11 INT,
-        stop_12 INT,
-        stop_13 INT,
-        stop_14 INT,
-        stop_15 INT,
-        stop_16 INT,
-        stop_17 INT
-        );
+        EXECUTE format('DROP TABLE IF EXISTS %I', c::text);
+        EXECUTE format(
+            'CREATE TABLE %I (
+                bird_name TEXT,
+                stop_1 INT,
+                stop_2 INT,
+                stop_3 INT,
+                stop_4 INT,
+                stop_5 INT,
+                stop_6 INT,
+                stop_7 INT,
+                stop_8 INT,
+                stop_9 INT,
+                stop_10 INT,
+                stop_11 INT,
+                stop_12 INT,
+                stop_13 INT,
+                stop_14 INT,
+                stop_15 INT,
+                stop_16 INT,
+                stop_17 INT
+            )',
+            c::text
     END LOOP;
 END
 $$
