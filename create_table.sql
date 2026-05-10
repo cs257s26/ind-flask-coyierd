981b94e73c9900f7a4d68d91f591fc28c517c7af
Dashiell Coyier <dashiellcoyier@Dashiells-MacBook-Pro-54.local>
2026-05-10 14:09:50 -0500

    adding back other years

--- create_table.sql
+++ create_table.sql
@@ -26,5 +26,53 @@ BEGIN
         stop_17 numeric
         );
     END LOOP;
+
+    FOR a IN 2008..2018 LOOP
+        DROP TABLE IF EXISTS a;
+        CREATE TABLE a (
+        bird_name text,
+        stop_1 numeric,
+        stop_2 numeric,
+        stop_3 numeric,
+        stop_4 numeric,
+        stop_5 numeric,
+        stop_6 numeric,
+        stop_7 numeric,
+        stop_8 numeric,
+        stop_9 numeric,
+        stop_10 numeric,
+        stop_11 numeric,
+        stop_12 numeric,
+        stop_13 numeric,
+        stop_14 numeric,
+        stop_15 numeric,
+        stop_16 numeric,
+        stop_17 numeric
+        );
+    END LOOP;
+
+    FOR a IN 2000..2004 LOOP
+        DROP TABLE IF EXISTS a;
+        CREATE TABLE a (
+        bird_name text,
+        stop_1 numeric,
+        stop_2 numeric,
+        stop_3 numeric,
+        stop_4 numeric,
+        stop_5 numeric,
+        stop_6 numeric,
+        stop_7 numeric,
+        stop_8 numeric,
+        stop_9 numeric,
+        stop_10 numeric,
+        stop_11 numeric,
+        stop_12 numeric,
+        stop_13 numeric,
+        stop_14 numeric,
+        stop_15 numeric,
+        stop_16 numeric,
+        stop_17 numeric
+        );
+    END LOOP;
 END
 $$
