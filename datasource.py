f26dcb87e3fff671d09f6f11e516b3c3448d5bfb
Dashiell Coyier <dashiellcoyier@Dashiells-MacBook-Pro-54.local>
2026-05-10 19:38:38 -0500

    datasource query update

--- datasource.py
+++ datasource.py
@@ -1,9 +1,3 @@
-"""psycopg2-sample.py
-
-Sample code demonstrating how to use the psycopg2 Python library to
-connect to a database and execute a query.
-"""
-
 import psycopg2 as ps
 
 import psqlConfig as config
@@ -11,8 +5,6 @@ import psqlConfig as config
 
 def connect():
     """Establishes a connection to the database with the following credentials:
-        user - username, which is also the name of the database
-        password - the password for this database on perlman
 
     Returns: a database connection.
 
@@ -31,15 +23,17 @@ def connect():
     return connection
 
 
-def get_sightings_at_stop_for_year(connection, bird, stop, year) -> list:
+def get_sightings_at_stop_for_year(connection, bird: str, stop: int, year: int) -> list:
     """Returns the amount of birds at a given stop at a given year.
 
     Args:
         connection (psycopg2.connection) - the connection to the database
-        temp (float) - the minimum high temperature
+        bird (str) - the bird in consideration
+        stop (int) - the stop on the route
+        year (int) - the year to find the sightings for
 
     Returns:
-        list - a list of all dates where the high temperature is greater or equal to temp, or None if the query fails.
+        list - a list with one item, the number of sightings at a single stop
     """
     try:
         cursor = connection.cursor()
@@ -53,15 +47,16 @@ def get_sightings_at_stop_for_year(connection, bird, stop, year) -> list:
         return None
 
 
-def get_all_bird_sightings_for_year(connection, bird, year) -> list:
-    """Returns the amount of birds at a given stop at a given year.
+def get_all_bird_sightings_for_year(connection, bird: int, year: str) -> list:
+    """Returns the amount of bird sightings for a given year across all stops.
 
     Args:
         connection (psycopg2.connection) - the connection to the database
-        temp (float) - the minimum high temperature
+        bird (str) - the bird in consideration
+        year (int) - the year to find the sightings for
 
     Returns:
-        list - a list of all dates where the high temperature is greater or equal to temp, or None if the query fails.
+        list - a list with one item, the number of sightings across all stops
     """
     try:
         cursor = connection.cursor()
@@ -99,7 +94,7 @@ def main():
 
     # Execute a simple query: how many earthquakes above the specified magnitude are there in the data?
     results = get_all_bird_sightings_for_year(
-        connection, "American Crow (Corvus brachyrhynchos) ", 2017
+        connection, "American Crow (Corvus brachyrhynchos) ", 2015
     )
 
     if results is not None:
@@ -112,15 +107,3 @@ def main():
 
 
 main()
-
-
-# SELECT column_name FROM information_schema.columns
-# ORDER BY SUM(column_name) DESC
-# LIMIT 1
-
-
-# SELECT (stop_1 + stop_2 + stop_3 + stop_4 + stop_5 + stop_6 + stop_7 + stop_8 + stop_9 + stop_10 + stop_11 + stop_12 + stop_13 + stop_14 + stop_15 + stop_16 + stop_17) FROM "2017" WHERE bird_name='American Crow (Corvus brachyrhynchos) ';
-# SELECT stop_1 + stop_1 FROM "2017" WHERE bird_name='American Crow (Corvus brachyrhynchos) ';
-
-# SELECT COALESCE(stop_1, 0) + COALESCE(stop_2, 0) + COALESCE(stop_3, 0) + COALESCE(stop_4, 0) + COALESCE(stop_5, 0) + COALESCE(stop_6, 0) + COALESCE(stop_7, 0) + COALESCE(stop_8, 0) + COALESCE(stop_9, 0) + COALESCE(stop_10, 0) + COALESCE(stop_11, 0) + COALESCE(stop_12, 0) + COALESCE(stop_13, 0) + COALESCE(stop_14, 0) + COALESCE(stop_15, 0) + COALESCE(stop_16, 0) + COALESCE(stop_17, 0) AS total_sightings FROM "2017" WHERE bird_name='American Crow (Corvus brachyrhynchos) ';
-# 'SELECT COALESCE(stop_1, 0) + COALESCE(stop_2, 0) + COALESCE(stop_3, 0) + COALESCE(stop_4, 0) + COALESCE(stop_5, 0) + COALESCE(stop_6, 0) + COALESCE(stop_7, 0) + COALESCE(stop_8, 0) + COALESCE(stop_9, 0) + COALESCE(stop_10, 0) + COALESCE(stop_11, 0) + COALESCE(stop_12, 0) + COALESCE(stop_13, 0) + COALESCE(stop_14, 0) + COALESCE(stop_15, 0) + COALESCE(stop_16, 0) + COALESCE(stop_17, 0) FROM "%s" WHERE bird_name=%s;'
